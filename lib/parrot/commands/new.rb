8bad47cc6ae2959d1104aa4bec4d6346585f50b7
Deepak Kumar <deepakkumarnd@gmail.com>
2026-09-23 15:36:24 +0530

    Fix tests for new command

--- lib/parrot/commands/new.rb
+++ lib/parrot/commands/new.rb
@@ -15,13 +15,12 @@ module Parrot
       end
 
       def run
-        config.logger.info "Creating new application #{app_root}"
-        skeleton_path = File.expand_path('../../../../skel', __FILE__)
-        config.logger.info "Using skeleten from #{skeleton_path}"
-
         if Dir.exist? @app_root
           raise "Directory #{@app_root} already exists"
         end
+
+        config.logger.info "Creating new blog at #{app_root}"
+        skeleton_path = File.expand_path('../../../../skel', __FILE__)
 
         config.logger.info "Copying skeleten from #{skeleton_path}"
         FileUtils.cp_r(skeleton_path, @app_root)
--- spec/parrot/commands/new_spec.rb
+++ spec/parrot/commands/new_spec.rb
@@ -8,15 +8,35 @@ describe Parrot::Commands do
       expect(Parrot::Commands::NewCommand.new(%w( foo ), config)).to respond_to(:run)
     end
 
-    it 'creates a blog application' do
-      Parrot::Commands::NewCommand.new(%w( blog ), config).run
-      expect(Dir.exist?('blog')).to be true
+    context 'Setup a blog' do
+      let(:app_name) { 'my-test-blog' }
 
-      file_list = %w[blog/css/app.scss blog/javascripts/app.js blog/images/favicon.ico blog/images/favicon.svg blog/images/apple-touch-icon.png blog/images/parrot.jpeg blog/views/layout.html.erb blog/views/404.md blog/views/posts/post1.md blog/views/posts/post2.md blog/config.yaml blog/public/.keep]
+      after(:each) {
+        FileUtils.rm_rf(app_name) if Dir.exist? app_name
+      }
 
-      expect(file_list.all? { |file| File.exist?(file) }).to be true
-      # cleanup
-      FileUtils.rm_rf('blog')
+      it 'Verify all files from skeleton template is copied correctly' do
+        Parrot::Commands::NewCommand.new([app_name], config).run
+        
+        expect(Dir.exist?(app_name)).to be true
+
+        file_list = [
+          "#{app_name}/config.yaml",
+          "#{app_name}/css/app.scss",
+          "#{app_name}/images/favicon.ico",
+          "#{app_name}/images/favicon.svg", 
+          "#{app_name}/images/apple-touch-icon.png",
+          "#{app_name}/images/parrot.jpeg",
+          "#{app_name}/javascripts/app.js",
+          "#{app_name}/public/.keep",
+          "#{app_name}/views/404.md",
+          "#{app_name}/views/layout.html.erb",
+          "#{app_name}/views/posts/about_parrot.md",
+          "#{app_name}/views/posts/sample.md"
+        ]
+
+        expect(file_list.all? { |file| File.exist?(file) }).to be true
+      end
     end
   end
 end
